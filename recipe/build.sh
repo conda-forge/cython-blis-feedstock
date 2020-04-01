dad60bccd2411467c025a21e0fb0d90d770f38a9
Jason Furmanek <furmanek@us.ibm.com>
2020-04-01 21:00:36 +0000

    fix migrator

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,10 @@
 
 export BLIS_COMPILER="$CC"
 if [[ "$(uname)" == "Linux" ]]; then
+    if [[ ${target_platform} =~ .*ppc.* ]] || [[ ${target_platform} =~ .*aarch64.* ]]; then
+        export BLIS_ARCH="generic"
+    fi
     $PYTHON -m pip install . --no-deps -vv --global-option="build_ext" --global-option="-lrt"
 else
-    $PYTHON -m pip install . --no-deps -vv 
+    $PYTHON -m pip install . --no-deps -vv
 fi
